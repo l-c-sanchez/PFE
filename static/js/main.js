0b384e9208185ed5bb7162c9b3f7e4d018f7df18
Léa Sanchez <sanchezlea.media@gmail.com>
2019-04-24 00:25:38 +0200

    add vue js

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+var example1 = new Vue({
+    el: '#example-1',
+    data: {
+      paras: [
+        { message: 'L’écran de l \'ordinateur montre une jeune femme en armure, un arc accroché dans le dos. “C’est l’un de mes personnages de jeu vidéo préférés”, confie Laurette en faisant pivoter, d’un geste sur son clavier, la guerrière juchée sur un promontoire rocheux. Etudiante en informatique de 20 ans, Laurette est passionnée par la populaire série de jeux vidéo d \'action et d’aventure Assassin’s Creed. Dans son dernier opus, les joueurs ont eu pour la première fois la possibilité d’incarner une héroïne - Kassandra.' },
+        { message: 'Le jeu a été présenté l’année dernière à l’E3, salon annuel de jeux vidéo très réputé. Que ce soit dans les jeux de tirs, comme Battlefield V où les joueurs peuvent désormais incarner une femme dans l’univers de la seconde guerre mondiale, ou même chez les Pokemon, puisqu’on peut désormais jouer une version femelle de Pikachu dans Super Smash Bros, jamais les personnages féminins n’ont semblé aussi présents. De quoi réjouir de nombreux observateurs, notamment dans la presse généraliste.' }
+      ]
+    }
+  })
